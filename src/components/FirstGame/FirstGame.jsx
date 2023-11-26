803d74ccf451a22a1c2fa6310a29932026f4a93a
Evhen Gaponyuk <e-gap@ukr.net>
2023-11-26 18:12:08 +0200

    random results

--- src/components/FirstGame/FirstGame.jsx
+++ src/components/FirstGame/FirstGame.jsx
@@ -6,19 +6,28 @@ import { useState } from "react";
 const FirstGame = () => {
   const [chosenColor, setChosenColor] = useState(null);
   const balls = ["green", "yellow", "red", "blue", "pink"];
+  const durations = ["6500ms", "7000ms", "7500ms", "8000ms", "8500ms"];
   const startGame = () => {
     console.log(chosenColor);
   };
   const ckickOnBall = (e) => {
     setChosenColor(e.currentTarget.getAttribute("color"));
   };
+  function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
 
   return (
     <div className={css.firstGame}>
       <Button text="start" handleButton={startGame} view="start_game" />
       <ul className={css.ballList}>
         {balls.map((item, index) => (
-          <OneBall key={index} color={item} handleClick={ckickOnBall} />
+          <OneBall
+            key={index}
+            color={item}
+            handleClick={ckickOnBall}
+            duration={shuffle(durations)[index]}
+          />
         ))}
       </ul>
     </div>
--- src/components/OneBall/OneBall.jsx
+++ src/components/OneBall/OneBall.jsx
@@ -1,12 +1,12 @@
 import css from "./OneBall.module.css";
 
-const OneBall = ({ color, handleClick }) => {
+const OneBall = ({ color, handleClick, duration }) => {
   const style = css[color];
   return (
     <li className={css.ballItem}>
       <div
         className={`${css.ball} ${style}`}
-        style={{ backgroundColor: color }}
+        style={{ backgroundColor: color, animationDuration: duration }}
         onClick={handleClick}
         color={color}
       ></div>
